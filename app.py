c1edf7bf2f206d04c7325ad619365d63dd036712
Vertocz <108529905+Vertocz@users.noreply.github.com>
2025-12-05 16:31:03 +0100

    retirer console

--- app.py
+++ app.py
@@ -287,7 +287,6 @@ def graph_suivi_forme(joueuse):
                 if st.button("✅ Oui, supprimer", key=f"conf_suppr_{row['id']}"):
                     try:
                         supabase.table("suivi_forme").delete().eq("id", row["id"]).execute()
-                        console.log(row, row["id"]
                         st.success("✅ Activité supprimée.")
                         st.rerun()
                     except Exception as e:
